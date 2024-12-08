9d16e0d54acaf80239259b4c784903227e3f3489
nikzan <nikzanvv@gmail.com>
2024-12-08 20:12:56 +0500

    Первый коммит

---
+++ build/Qt_6_8_1_for_macOS-Debug/qtcsettings.cmake
@@ -0,0 +1,10 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("QT_QMAKE_EXECUTABLE" "/Users/nikzan/Qt/6.8.1/macos/bin/qmake" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_FLAGS_INIT" "-DQT_QML_DEBUG" CACHE "STRING" "" FORCE)
+set("CMAKE_C_COMPILER" "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
+set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "/Users/nikzan/QtProjects/BullsAndCows/build/Qt_6_8_1_for_macOS-Debug/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_PREFIX_PATH" "/Users/nikzan/Qt/6.8.1/macos" CACHE "PATH" "" FORCE)
+set("CMAKE_CXX_COMPILER" "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang++" CACHE "FILEPATH" "" FORCE)
---
+++ cmake-build-debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_BullsAndCows_FILE /Users/nikzan/QtProjects/BullsAndCows/cmake-build-debug/BullsAndCows.app/Contents/MacOS/BullsAndCows)
+set(__QT_DEPLOY_TARGET_BullsAndCows_TYPE EXECUTABLE)
